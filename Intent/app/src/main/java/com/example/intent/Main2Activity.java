e589f390e2aa14f6ad8a1f24088ba971bf386a6a
rizkyanugrah <rizkyanugrah0331@gmail.com>
2020-08-25 20:05:58 +0700

    tugas 6

---
+++ Intent/app/src/main/java/com/example/intent/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.example.bundle;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void oncCeate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        set
+    }
+}
