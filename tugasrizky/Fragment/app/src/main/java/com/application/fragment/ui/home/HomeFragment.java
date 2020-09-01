be17646664c6fdfaf6d63b8cd41d64752f48a669
rizkyanugrah <rizkyanugrah0331@gmail.com>
2020-09-01 17:41:35 +0700

    Tugas 7

---
+++ tugasrizky/Fragment/app/src/main/java/com/application/fragment/ui/home/HomeFragment.java
@@ -0,0 +1,21 @@
+package com.application.fragment.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import com.application.fragment.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
---
+++ tugasrizky/Fragment/app/src/main/java/com/application/fragment/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,21 @@
+package com.application.fragment.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import com.application.fragment.R;
+
+public class NotificationsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup
+            container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, null);
+    }
+}
